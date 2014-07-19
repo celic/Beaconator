e45d6715629cc3fe3101387ce5c37f0143ce336c
Andrew Rempe <andrewrempe@gmail.com>
2014-07-19 14:49:34 -0400

    Rearrange ping_beacon

--- ping_beacon.rb
+++ ping_beacon.rb
@@ -1,18 +1,18 @@
 require 'net/http'
 require 'nokogiri'
 
-# Clear previous output
-file = File.open "beacon", "w"
-file.truncate 0
-
 # Beacon url
-url = 'https://beacon.nist.gov/rest/record/last'
+url = 'http://beacon.nist.gov/rest/record/last'
 
 # Get Beacon data
 xml = Net::HTTP.get_response(URI.parse(url)).body
 
 # Parse outputValue from xml
 doc = Nokogiri::XML(xml)
+
+# Clear previous output
+file = File.open "beacon", "w"
+file.truncate 0
 
 # Output beacon to file
 puts doc.at_xpath('//record').at_xpath('//outputValue').content.to_i 16
