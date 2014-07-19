02b980c865dd0a03138b1e91f9542b126a303d4b
celic <cceli@codequarry.net>
2014-07-18 20:55:13 -0400

    Allows user to add options

--- beaconator.js
+++ beaconator.js
@@ -1,1 +1,18 @@
 /* Beaconator js */
+$(document).ready(function(){
+
+	$options_list = $('#options_list');
+	$add_option = $('#add_option');
+	var num_options = 2;
+	var options = [];
+
+	// Adds a new option to the list
+	$add_option.click(function(){
+
+		// Increment counter
+		num_options++;
+
+		// Add preconfigured html to the page
+		$options_list.append("<div><label>Option " + num_options + "</label><input type='text' name='Option" + num_options + "' /></div>");
+	});
+});
