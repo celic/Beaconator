fdbb889a8a17e109a6fdfeb0ee32a59b953d5196
celic <cceli@codequarry.net>
2014-07-19 12:53:39 -0400

    Allows for a port argument

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'webrick'
 
-server = WEBrick::HTTPServer.new :Port => 8000
+server = WEBrick::HTTPServer.new :Port => ARGV[0]
 server.mount "/", WEBrick::HTTPServlet::FileHandler, './'
 
 trap('INT') { server.stop }
